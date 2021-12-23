fdc10b26c44bb77a6f5c55cc6160ff851db1f231
Avinash <Avichauhan111196@gmail.com>
2021-12-23 10:04:40 +0530

    Added : Employee Wages added

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -5,15 +5,22 @@ import java.util.Random;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int present=1;
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHours;
+        int empWage;
         Random random = new Random();
         int empCheck = random.nextInt(2);
-        if(empCheck==present){
+        if(empCheck==IS_FULL_TIME){
+            empHours=8;
             System.out.println("Employee is Present");
         }
         else {
+            empHours=0;
             System.out.println("Employee is absent");
         }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage of employee is" +empWage);
     }
 
 }
