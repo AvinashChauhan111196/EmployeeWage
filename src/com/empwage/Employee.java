b0217548f0571df2811a405a211a053fc53373fc
Avinash <Avichauhan111196@gmail.com>
2021-12-23 09:43:59 +0530

    Added : Employee Attendance added

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -1,8 +1,19 @@
 package com.empwage;
 
+import java.util.Random;
+
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
+        int present=1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if(empCheck==present){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 
 }
