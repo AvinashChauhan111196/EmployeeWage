76ffa1ae5b25cfaac03706ac7da6a11aef2e1e4c
Avinash <Avichauhan111196@gmail.com>
2021-12-24 09:33:23 +0530

    Modified : Switch case

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -3,27 +3,27 @@ package com.empwage;
 import java.util.Random;
 
 public class Employee {
+
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int EMP_RATE_PER_HOUR=20;
         int empHours;
         int empWage;
         Random random = new Random();
         int empCheck = random.nextInt(3);
 
-        if(empCheck==IS_FULL_TIME){
-            empHours=8;
-            System.out.println("Employee is Present");
-        }
-        else if(empCheck==IS_PART_TIME){
-            empHours=4;
-            System.out.println("Part time Employee");
-        }
-        else {
-            empHours=0;
-            System.out.println("Employee is absent");
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHours=8;
+                break;
+            case IS_PART_TIME:
+                empHours=4;
+                break;
+            default:
+                empHours=0;
         }
 
         empWage=empHours*EMP_RATE_PER_HOUR;
