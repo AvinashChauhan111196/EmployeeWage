14b3c649b6b9c90c99f383023fc84ca95fe85963
Avinash <Avichauhan111196@gmail.com>
2022-01-03 13:42:37 +0530

    Added : Wage for month

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -8,12 +8,15 @@ public class Employee {
     public static final int IS_PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int Num_OF_WORKING_DAYS=2;
+    public static final int MAX_HRS_IN_MONTH=10;
 
     public static void main(String[] args) {
 
         int empHours = 0,empWage = 0,totalEmpWage=0;
+        int totalEmpHrs=0, totalWorkingDays=0;
 
-        for(int day=0;day<Num_OF_WORKING_DAYS;day++) {
+        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays <Num_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -25,11 +28,10 @@ public class Employee {
                 default:
                     empHours = 0;
             }
-            empWage = empHours * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage:" + empWage);
+            totalEmpHrs+=empHours;
+            System.out.println("Day: " + totalWorkingDays +" Emp hours " +empHours);
         }
-
-    System.out.println("Total Emp Wage" + totalEmpWage);
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+    System.out.println(" Total Emp Wage" + totalEmpWage);
     }
 }
