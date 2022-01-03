b0fdc172cec8c605473da75e5698bec3a8d89032
Avinash <Avichauhan111196@gmail.com>
2022-01-03 13:27:11 +0530

    Added : Wages functionality added

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -7,27 +7,29 @@ public class Employee {
     public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
+    public static final int Num_OF_WORKING_DAYS=2;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
-        int empHours;
-        int empWage;
-        Random random = new Random();
-        int empCheck = random.nextInt(3);
 
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHours=8;
-                break;
-            case IS_PART_TIME:
-                empHours=4;
-                break;
-            default:
-                empHours=0;
+        int empHours = 0,empWage = 0,totalEmpWage=0;
+
+        for(int day=0;day<Num_OF_WORKING_DAYS;day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage:" + empWage);
         }
 
-        empWage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of employee is" +empWage);
+    System.out.println("Total Emp Wage" + totalEmpWage);
     }
-
 }
